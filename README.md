b9a4d8186e1263eb74c75dae0d8becbba8e23743
Sidharth Mishra <sidmishraw@gmail.com>
2017-09-22 18:50:42 -0700

    WHILE language first draft implemented. Working fine for my test cases. TODO:: test professor Austin's test cases and implement it in Haskell.
    Signed-off-by: Sidharth Mishra <sidmishraw@gmail.com>

--- README.md
+++ README.md
@@ -1,55 +1,110 @@
-# The BOOL* language implementation using Scala
+# The WHILE language
 
-This is the language implementation for **`BOOL*`**
+This is the language implementation for **`WHILE`** language using Scala programming language.
+
+This language unlike BOOL and BOOL++, supports while loop and mutable state.
 
 ## Language Specification:
 
 ```
-e ::= True
-    | False
-    | n
-    | if e then e else e
-    | succ e
-    | pred e
+e ::= x
+    | v
+    | x := e
+    | e; e
+    | e op e
+    | if e then e else e 
+    | while (e) e
+    | not e
 ```
 
 ```
 v ::= True
     | False
     | n
+    
+op := +
+    | -
+    | *
+    | /
+    | >
+    | <
+    | <=
+    | >=
+    | and
+    | or
 ```
 
-## Big Step operational semantics:
+## Small Step Operational Semantics
 
-* Rule#1
 ```
-    e1 ⇓ True       e2 ⇓ v
-⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯       [BS - IF - True]
-if e1 then e2 else e3 ⇓ v
-```
+[SS-VALUE]               -----------------------------
+                            v, σ -> v, σ
 
-* Rule#2
-```
-    e1 ⇓ False      e3 ⇓ v
-⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯      [BS - IF - False]
-if e1 then e2 else e3 ⇓ v
-```
 
-* Rule#3
-```
-v ⇓ v
-```
+                            x ∈ domain(σ)  σ(x) = v
+[SS-VAR]                 -----------------------------
+                            x, σ -> v, σ
 
-* Rule#4
-```
- e ⇓ n
-⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯                      [BS - Succ]
-succ e ⇓ n + 1
-```
 
-* Rule#4
-```
- e ⇓ n
-⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯                      [BS - Pred]
-pred e ⇓ n - 1
+[SS-ASSIGN-REDUCTION]    -----------------------------
+                            x := v, σ -> v, σ[x:=v]
+
+
+                            e, σ -> e', σ'
+[SS-ASSIGN-CONTEXT]      -----------------------------
+                            x := e, σ -> x := e', σ'
+
+
+[SS-SEQ-REDUCTION]       -----------------------------
+                            v; e , σ -> e, σ
+
+
+                            e1, σ -> e1', σ'
+[SS-SEQ-CONTEXT]         -----------------------------
+                            e1; e2, σ -> e1'; e2, σ'
+
+
+[SS-IF-TRUE-REDUCTION]   --------------------------------------
+                            if True then e1 else e2, σ -> e1, σ
+
+
+[SS-IF-FALSE-REDUCTION]  --------------------------------------
+                            if False then e1 else e2, σ -> e2, σ
+
+
+                                e1, σ -> e1', σ'
+[SS-IF-CONTEXT]          -----------------------------------------------------------
+                            if e1 then e2 else e3, σ -> if e1' then e2 else e3, σ'
+
+
+                                v = apply op v1 v2
+[SS-OP-REDUCTION]        ---------------------------------------------------
+                               v1 op v2, σ -> v, σ
+
+
+                                e, σ -> e', σ'
+[SS-OP-CONTEXT#2]        ---------------------------------------------------
+                               v op e, σ -> v op e', σ'
+
+
+                                e1, σ -> e1', σ'
+[SS-OP-CONTEXT#1]        ---------------------------------------------------
+                               e1 op e2, σ -> e1' op e2, σ'
+
+
+[SS-WHILE]               ------------------------------------------------------------------
+                            while(e1) e2, σ -> if e1 then (e2; while(e1) e2) else false, σ
+
+                            
+[SS-NOT-TRUE]            -----------------------------------   
+                            not True, σ -> False, σ
+
+
+[SS-NOT-FALSE]           -----------------------------------   
+                            not False, σ -> True, σ
+
+
+                            e, σ -> e', σ'
+[SS-NOT-CONTEXT]         -----------------------------------
+                            not e, σ -> not e', σ'
 ```
--- src/main/scala/whilelang/WhilelangDriver.scala
+++ src/main/scala/whilelang/WhilelangDriver.scala
@@ -23,6 +23,7 @@ import scala.collection.mutable
   *         | e op e                    // binary operations
   *         | if e then e else e        // if then else
   *         | while(e) e                // while loops
+  *         | not e                     // negation - unary operator
   *
   *         v := True
   *         | False
@@ -36,6 +37,8 @@ import scala.collection.mutable
   *         | <
   *         | >=
   *         | <=
+  *         | and
+  *         | or
   *
   *
   *
@@ -49,66 +52,161 @@ object WhilelangDriver {
     /**
       * String to show, a Haskell like Show typeclass
       *
-      * @param e
-      * @return
+      * @param e the expression
+      * @return the string representation of the expression
       */
     def show(e: Exp): String = e match {
+    
         case VTrue => "True"
         case VFalse => "False"
-        case VInt(i) => s"VInt($i)"
-        case Eif(e1, e2, e3) => s"if ${show(e1)} then ${show(e2)} else ${show(e2)}"
+        case VInt(i) => s"$i"
+    
+        case Eif(e1, e2, e3) => s"if ${show(e1)} then ${show(e2)} else ${show(e3)}"
+        
         case Ea(name) => name
+    
         case :=(a, e) => s"${show(a)} := ${show(e)}"
-        case ::(e1, e2) => s"${show(e1)};${show(e2)}"
+    
+        case ::(e1, e2) => s"${show(e1)}; ${show(e2)}"
+        
         case While(e1, e2) => s"while(${show(e1)}) ${show(e2)}"
-        case +(e1, e2) => s"${show(e1)} +  ${show(e2)}"
-        case -(e1, e2) => s"${show(e1)} -  ${show(e2)}"
-        case *(e1, e2) => s"${show(e1)} *  ${show(e2)}"
-        case /(e1, e2) => s"${show(e1)} /  ${show(e2)}"
-        case <(e1, e2) => s"${show(e1)} <  ${show(e2)}"
-        case >(e1, e2) => s"${show(e1)} >  ${show(e2)}"
-        case >=(e1, e2) => s"${show(e1)} <=  ${show(e2)}"
-        case <=(e1, e2) => s"${show(e1)} >=  ${show(e2)}"
+    
+        case Op("+", e1, e2) => s"${show(e1)} + ${show(e2)}"
+        case Op("-", e1, e2) => s"${show(e1)} - ${show(e2)}"
+        case Op("*", e1, e2) => s"${show(e1)} * ${show(e2)}"
+        case Op("/", e1, e2) => s"${show(e1)} / ${show(e2)}"
+        case Op(">", e1, e2) => s"${show(e1)} > ${show(e2)}"
+        case Op("<", e1, e2) => s"${show(e1)} < ${show(e2)}"
+        case Op(">=", e1, e2) => s"${show(e1)} >= ${show(e2)}"
+        case Op("<=", e1, e2) => s"${show(e1)} <= ${show(e2)}"
+        case Op("and", e1, e2) => s"${show(e1)} and ${show(e2)}"
+        case Op("or", e1, e2) => s"${show(e1)} or ${show(e2)}"
     }
     
     /**
-      * Evaluates the expression -- lets assume we are using BigStep semantics here
-      *
-      * Caveat - I don't know how it should look in case of small step semantics
+      * Evaluates the expression -- lets assume we are using Small step semantics here
       *
       * @param e the expression
-      * @return the value of the expression after evaluation
+      * @return another expression
       */
-    def evaluate(e: Exp): ExpValue = e match {
-        
+    def evaluate(e: Exp): Exp = e match {
+    
+        // SS-VALUE
         // value cases, these are normalized and cannot be processed further
-        // no updates to the store(?)
+        // no updates to the store
         case VTrue => VTrue
         case VFalse => VFalse
         case VInt(i) => VInt(i)
-        
-        // if condition
+
+        // SS-IF-CONDN
+        // `if condn context: SS-IF-Context`
+        // if e1 then e2 else e3, store -> if e1' then e2 else e3, store'
         // store might get updated when evaluating the conditions
-        case Eif(e1, e2, e3) => evaluate(e1) match {
-            case VTrue => evaluate(e2)
-            case VFalse => evaluate(e3)
-            case _ => throw new Exception("operation is not supported")
+        // SS-IF-True reduction
+        // `if True then e1 else e2, store -> e1, store`
+        // SS-IF-True reduction
+        // `if False then e1 else e2, store -> e2, store`
+        case Eif(VTrue, e, _) => e
+
+        case Eif(VFalse, _, e) => e
+
+        case Eif(e1, e2, e3) => Eif(evaluate(e1), e2, e3)
+
+        // SS-VAR
+        case Ea(name) => if (store.contains(name)) store(name) else error("not in store")
+
+        // SS-ASSIGN
+        // SS-ASSIGN-REDUCTION
+        // x := v, store -> v, store(x:=v)
+        case :=(a, v: ExpValue) => {
+            // store and return
+            store(a.name) = v
+            v
         }
-        
-        // SS-store-access-reduction
-        case Ea(name) => if (store.contains(name)) store(name) else throw new Exception("Not in store")
-        
-        // SS-assign-context
-        case :=(a, e: ExpValue) => {
-            // evaluate e to e' and store to store'
-            // val ee =
-            e
+
+        // SS-ASSIGN-CONTEXT
+        // x := e, store -> x := e', store' || where, e, store -> e', store'
+        case :=(a, e) => :=(a, evaluate(e))
+
+        // SS-SEQ
+        // SS-SEQ-REDUCTION
+        // v; e, store -> e, store
+        case ::(_: ExpValue, e) => e
+
+        // SS-SEQ-CONTEXT
+        // e1;e2, store -> e1';e2, store' || where e1, store -> e1', store'
+        case ::(e1, e2) => ::(evaluate(e1), e2)
+
+        // SS-OP
+        // SS-OP-REDUCTION
+        case x@Op(s, v1: ExpValue, v2: ExpValue) => x.apply(s, v1, v2)
+
+        // SS-OP-CONTEXT #2
+        // v op e, store -> v op e', store' || where e, store -> e', store'
+        case Op(s, v: ExpValue, e) => Op(s, v, evaluate(e))
+
+        // SS-OP-CONTEXT #1
+        // e1 op e2, store -> e1' op e2, store' || where e1, store -> e1', store'
+        case Op(s, e1, e2) => Op(s, evaluate(e1), e2)
+
+        // SS-NOT-REDUCTION-TRUE
+        // not True, store -> False, store
+        // not False, store -> True, store
+        case not(VTrue) => VFalse
+        case not(VFalse) => VTrue
+        case not(VInt(_)) => error("not supported")
+
+        // SS-NOT-CONTEXT
+        // not e, store -> not e', store' || where e, store -> e', store'
+        case not(e) => not(evaluate(e))
+
+        //SS-WHILE-CONTEXT
+        // while(e1) e2, store -> if e1 then (e2; while(e1) e2) else False, store
+        // since the expression has been rewritten as a recursive `if expression`
+        // it actually simplifies the expression
+        case While(e1, e2) => Eif(e1, ::(e2, While(e1, e2)), VFalse)
+    }
+    
+    /**
+      * Process recursively solving the problems
+      *
+      * @param e the expression to solve
+      * @return the value of the expression/program
+      */
+    def process(e: Exp): ExpValue = e match {
+        case x: ExpValue => x
+        case x: Exp => {
+            println(s"Intermediate step :: ${show(x)}")
+            process(evaluate(x))
         }
     }
     
     def main(args: Array[String]): Unit = {
-        
-        val prog1 = ::(:=(Ea("a"), VInt(5)), While(VTrue, WhilelangDriver.+.apply(Ea("a"), VInt(2))))
+    
+        // myVar := 5; kk := 1; while(myVar > 0) (kk := kk + 1; myVar := myVar - 1); kk
+        val line1 = :=(Ea("myVar"), VInt(5))
+        val line2 = :=(Ea("kk"), VInt(1))
+        val line5 = :=(Ea("kk"), Op("+", Ea("kk"), VInt(1)))
+        val line6 = :=(Ea("myVar"), Op("-", Ea("myVar"), VInt(1)))
+        val line7 = Ea("kk")
+        val line4 = ::(line5, line6)
+        val line3 = While(Op(">", Ea("myVar"), VInt(0)), line4)
+    
+        val prog1 = ::(::(::(line1, line2), line3), line7)
+    
+        val prog2 = ::(:=(Ea("a"), VInt(5)), Op("+", Ea("a"), VInt(10)))
+    
+        println("---------------------------------------")
+        println(s"SHOW :: ${show(prog1)}")
+        println("---------------------------------------")
+        println(s"Result prog1 :: ${show(process(prog1))}")
+        println("---------------------------------------")
+    
+        println("---------------------------------------")
+        println(s"SHOW :: ${show(prog2)}")
+        println("---------------------------------------")
+        println(s"Result prog2 :: ${show(process(prog2))}")
+        println("---------------------------------------")
     }
     
     /**
@@ -122,19 +220,84 @@ object WhilelangDriver {
     abstract class ExpValue extends Exp
     
     /**
+      * Error handler
+      *
+      * @param msg the message to be displayed for the error
+      */
+    def error(msg: String): ExpValue = {
+        
+        throw new Exception(msg)
+    }
+    
+    /**
       * Operations
       *
       * @param e1 left expression
       * @param e2 right expression
       */
-    abstract class Op(e1: Exp, e2: Exp) extends Exp
+    case class Op(s: String, e1: Exp, e2: Exp) extends Exp {
     
-    /**
-      * The variable/address location
-      *
-      * @param name
-      */
-    case class Ea(name: String) extends Exp
+        // application of the operation on the operands
+        def apply(s: String, v1: ExpValue, v2: ExpValue): ExpValue = {
+        
+            s match {
+            
+                case "+" => (v1, v2) match {
+                    case (VInt(i), VInt(j)) => VInt(i + j)
+                    case _ => error("not supported for the value!")
+                }
+            
+                case "-" => (v1, v2) match {
+                    case (VInt(i), VInt(j)) => VInt(i - j)
+                    case _ => error("not supported for the value!")
+                }
+            
+                case "*" => (v1, v2) match {
+                    case (VInt(i), VInt(j)) => VInt(i * j)
+                    case _ => error("not supported for the value!")
+                }
+            
+                case "/" => (v1, v2) match {
+                    case (VInt(i), VInt(j)) => VInt(i / j)
+                    case _ => error("not supported for the value!")
+                }
+            
+                case ">" => (v1, v2) match {
+                    case (VInt(i), VInt(j)) => if (i > j) VTrue else VFalse
+                    case _ => error("not supported for the value!")
+                }
+            
+                case ">=" => (v1, v2) match {
+                    case (VInt(i), VInt(j)) => if (i >= j) VTrue else VFalse
+                    case _ => error("not supported for the value!")
+                }
+            
+                case "<" => (v1, v2) match {
+                    case (VInt(i), VInt(j)) => if (i < j) VTrue else VFalse
+                    case _ => error("not supported for the value!")
+                }
+            
+                case "<=" => (v1, v2) match {
+                    case (VInt(i), VInt(j)) => if (i <= j) VTrue else VFalse
+                    case _ => error("not supported for the value!")
+                }
+            
+                case "and" => (v1, v2) match {
+                    case (VTrue, VTrue) => VTrue
+                    case (_, VFalse) => VFalse
+                    case (VFalse, _) => VFalse
+                    case (_, _) => error("not supported")
+                }
+            
+                case "or" => (v1, v2) match {
+                    case (VFalse, VFalse) => VFalse
+                    case (_, VTrue) => VTrue
+                    case (VTrue, _) => VTrue
+                    case (_, _) => error("not supported")
+                }
+            }
+        }
+    }
     
     /**
       * Integer value
@@ -169,28 +332,19 @@ object WhilelangDriver {
     case class Eif(e1: Exp, e2: Exp, e3: Exp) extends Exp
     
     /**
+      * The variable/address location
+      *
+      * @param name the name of the variable
+      */
+    case class Ea(name: String) extends Exp
+    
+    /**
       * While loop
       *
       * @param e1 the condition
       * @param e2 the while loop body containing expressions that are iterated
       */
     case class While(e1: Exp, e2: Exp) extends Exp
-    
-    case class +(e1: Exp, e2: Exp) extends Op(e1, e2)
-    
-    case class -(e1: Exp, e2: Exp) extends Op(e1, e2)
-    
-    case class *(e1: Exp, e2: Exp) extends Op(e1, e2)
-    
-    case class /(e1: Exp, e2: Exp) extends Op(e1, e2)
-    
-    case class >(e1: Exp, e2: Exp) extends Op(e1, e2)
-    
-    case class >=(e1: Exp, e2: Exp) extends Op(e1, e2)
-    
-    case class <(e1: Exp, e2: Exp) extends Op(e1, e2)
-    
-    case class <=(e1: Exp, e2: Exp) extends Op(e1, e2)
     
     /**
       * True value
@@ -202,4 +356,10 @@ object WhilelangDriver {
       */
     case object VFalse extends ExpValue
     
+    /**
+      * negation
+      *
+      * @param e the expression being negated
+      */
+    case class not(e: Exp) extends Exp
 }
